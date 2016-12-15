0492599bbaf9890c79654c9dac247e4e3db34406
Taryn Hill <taryn@phrohdoh.com>
2016-12-15 10:42:24 -0600

    Refactor the REPL a bit

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,19 +1,18 @@
+extern crate pest;
+use pest::StringInput;
+
 extern crate tsqlust;
 use tsqlust::Rdp;
 
 use std::io::{self, BufRead, Write};
 
-extern crate pest;
-use pest::StringInput;
-
 fn main() {
     let stdin = io::stdin();
     println!("Enter 'q' at any time to quit.");
     println!("Enter '?' at any time for help.");
+    println!();
 
     loop {
-        println!();
-
         print!(">> ");
         io::stdout().flush().expect("Failed to flush stdout");
 
@@ -21,22 +20,35 @@ fn main() {
         stdin.lock().read_line(&mut line).expect("Failed to read from stdin");
         line = line.trim().to_string();
 
-        if line == "q" {
-            println!("Goodbye!");
-            break;
-        } else if line == "?" {
-            println!("Type 'q' at any time to quit.");
+        if line.is_empty() {
             continue;
         }
 
-        let mut parser = Rdp::new(StringInput::new(&line));
-        if !parser.tsql() {
-            println!("Invalid TSQL (TODO: explain why)");
-            println!("Currently this REPL only accepts valid SELECT statements.");
-            println!("Try: SELECT * FROM YourTableName");
-            continue;
+        let line_str = line.as_str();
+        match line_str {
+            "help" | "?" | "/h" | "/?" => {
+                println!("Type 'q' at any time to quit.");
+                continue;
+            }
+            "q" | "quit" => {
+                println!("Goodbye!");
+                break;
+            }
+            _ => {
+                let mut parser = Rdp::new(StringInput::new(line_str));
+                print_ast(&mut parser);
+            }
         }
-
-        println!("{:#?}", parser.parse_stmt_select());
     }
+}
+
+fn print_ast(parser: &mut Rdp<StringInput>) {
+    if !parser.tsql() {
+        println!("Invalid TSQL (TODO: explain why)");
+        println!("Currently this REPL only accepts valid SELECT statements.");
+        println!("Try: SELECT * FROM YourTableName");
+        return;
+    }
+
+    println!("{:#?}", parser.parse_stmt_select());
 }
