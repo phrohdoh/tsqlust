3adf9883f670ed7839ff56a6aea27048ab2b7ec8
Taryn Hill <taryn@phrohdoh.com>
2017-01-26 21:59:45 -0600

    Break code up a bit more

--- src/jsbindings/mod.rs
+++ src/jsbindings/mod.rs
@@ -5,34 +5,41 @@ use ::get_diagnostics_for_tsql as lib_get_diagnostics;
 use neon::js::{JsString, JsNumber, JsObject, JsArray, Object};
 use neon::vm::{Call, JsResult};
 
-pub fn get_diagnostics_for_tsql(call: Call) -> JsResult<JsArray> {
+pub fn get_diagnostics_for_tsql<'a>(call: &'a mut Call) -> JsResult<'a, JsArray> {
     let tsql = call.arguments.require(call.scope, 0)?.check::<JsString>()?.value();
 
     let mut vis = visitors::SameLineTopStmtParens {};
     let diagnostics = lib_get_diagnostics(&tsql, &mut vis).expect("Failed to get diagnostics");
-    vec_diags_to_jsarray(diagnostics, call)
+    convert_diagnostics_to_jsarray(diagnostics, call)
 }
 
-pub fn vec_diags_to_jsarray(diagnostics: Vec<diagnostics::Diagnostic>,
-                            call: Call)
-                            -> JsResult<JsArray> {
+pub fn convert_diagnostics_to_jsarray<'a>(diagnostics: Vec<diagnostics::Diagnostic>,
+                                          call: &'a mut Call)
+                                          -> JsResult<'a, JsArray> {
     let len = diagnostics.len() as u32;
     let arr = JsArray::new(call.scope, len);
 
-    for (idx, diag) in diagnostics.iter().enumerate() {
-        let obj = JsObject::new(call.scope);
-
-        let pos_line = JsNumber::new(call.scope, diag.pos.line as f64);
-        let pos_col = JsNumber::new(call.scope, diag.pos.col as f64);
-        let code = JsString::new(call.scope, &diag.code).unwrap();
-        let message = JsString::new(call.scope, &diag.message).unwrap();
-
-        obj.set("pos_line", pos_line)?;
-        obj.set("pos_col", pos_col)?;
-        obj.set("code", code)?;
-        obj.set("message", message)?;
-        arr.set(idx as u32, obj)?;
+    for (idx, diagnostic) in diagnostics.into_iter().enumerate() {
+        let item = convert_diagnostic_to_jsobject(diagnostic, call).unwrap_or_else(|_| panic!());
+        arr.set(idx as u32, item)?;
     }
 
     Ok(arr)
+}
+
+pub fn convert_diagnostic_to_jsobject<'a>(diagnostic: diagnostics::Diagnostic,
+                                          call: &'a mut Call)
+                                          -> JsResult<'a, JsObject> {
+    let obj = JsObject::new(call.scope);
+
+    let pos_line = JsNumber::new(call.scope, diagnostic.pos.line as f64);
+    let pos_col = JsNumber::new(call.scope, diagnostic.pos.col as f64);
+    let code = JsString::new(call.scope, &diagnostic.code).unwrap();
+    let message = JsString::new(call.scope, &diagnostic.message).unwrap();
+
+    obj.set("pos_line", pos_line)?;
+    obj.set("pos_col", pos_col)?;
+    obj.set("code", code)?;
+    obj.set("message", message)?;
+    Ok(obj)
 }
