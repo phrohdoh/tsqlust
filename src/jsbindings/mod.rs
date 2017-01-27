137ef0ee3b97cf57128eecd171c6349a00da0339
Taryn Hill <taryn@phrohdoh.com>
2017-01-26 21:04:12 -0600

    Sprinkle more lifetimes around

--- src/jsbindings/mod.rs
+++ src/jsbindings/mod.rs
@@ -1,12 +1,13 @@
 use diagnostics;
-use ::get_diagnostics_for_tsql as lib_get_diagnostics;
 use visitors;
+use ::get_diagnostics_for_tsql as lib_get_diagnostics;
 
 use neon::js::{JsString, JsNumber, JsObject, Object};
 use neon::vm::Call;
 use neon::mem::Handle;
+use neon::scope::Scope;
 
-pub fn get_diagnostics_for_tsql(call: Call) -> Vec<diagnostics::Diagnostic> {
+pub fn get_diagnostics_for_tsql<'a>(call: &'a mut Call) -> Vec<Handle<'a, JsObject>> {
     let tsql = call.arguments
         .require(call.scope, 0)
         .unwrap_or_else(|_| panic!())
@@ -15,18 +16,21 @@ pub fn get_diagnostics_for_tsql(call: Call) -> Vec<diagnostics::Diagnostic> {
         .value();
 
     let mut vis = visitors::SameLineTopStmtParens {};
-    lib_get_diagnostics(&tsql, &mut vis).expect("Failed to get diagnostics")
+    lib_get_diagnostics(&tsql, &mut vis)
+        .expect("Failed to get diagnostics")
+        .iter()
+        .map(|d| d.to_jsobject(call))
+        .collect::<Vec<_>>()
 }
 
 impl diagnostics::Diagnostic {
-    pub fn to_jsobject(self, call: Call) -> Handle<JsObject> {
-        let scope = call.scope;
-        let obj = JsObject::new(scope);
+    pub fn to_jsobject<'a>(&self, call: &'a mut Call) -> Handle<'a, JsObject> {
+        let obj = JsObject::new(call.scope);
 
-        let pos_line = JsNumber::new(scope, self.pos.line as f64);
-        let pos_col = JsNumber::new(scope, self.pos.col as f64);
-        let code = JsString::new(scope, &self.code).unwrap();
-        let message = JsString::new(scope, &self.message).unwrap();
+        let pos_line = JsNumber::new(call.scope, self.pos.line as f64);
+        let pos_col = JsNumber::new(call.scope, self.pos.col as f64);
+        let code = JsString::new(call.scope, &self.code).unwrap();
+        let message = JsString::new(call.scope, &self.message).unwrap();
 
         let _ = obj.set("pos_line", pos_line);
         let _ = obj.set("pos_col", pos_col);
